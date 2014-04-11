ec7795559a67c7de881fb4fdf24e65b608e1d326
Danny Swaby <danny@swa.by>
2014-04-11 12:33:14 -0500

    added test for --empty option

--- test/test-creation.js
+++ test/test-creation.js
@@ -57,4 +57,39 @@ describe('marionette-coffee generator', function () {
       done();
     });
   });
+
+it('creates expected files when empty option is used', function (done) {
+    var expected = [
+      // add files you expect to exist here.,
+     '.editorconfig',
+     '.bowerrc',
+     '.gitignore',
+     'package.json',
+     'bower.json',
+     'Gruntfile.js',
+     'README.md',
+     'app/r.js',
+     'app/favicon.ico',
+     'app/404.html',
+     'app/assets/css/app.css',
+     'app/assets/require_main.coffee',
+     'test/SpecRunner.coffee',
+     'app/assets/build.js',
+     'server/app.js',
+     'test/TestRunner.html',
+     'test/coffee/TestSpec.coffee',
+     'test/hello.js',
+     'app/template.html',
+     'app/assets/coffee/app.coffee'
+    ];
+
+    helpers.mockPrompt(this.app, {
+      'empty': true
+    });
+    this.app.options['skip-install'] = true;
+    this.app.run({}, function () {
+      helpers.assertFile(expected);
+      done();
+    });
+  });
 });
